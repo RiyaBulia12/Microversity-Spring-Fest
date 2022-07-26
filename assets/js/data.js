0e8d2376a68be79373679890fec6ceb146c6ae8b
Apple <riyabulia12@gmail.com>
2022-07-26 17:40:47 +0530

    Add event and guest data object

---
+++ assets/js/data.js
@@ -0,0 +1,65 @@
+export const eventList = [{
+  icon: 'assets/img/icons/icons8-documentary-100.png',
+  title: 'Film Fest',
+  subtitle: 'A Picture Tale is the perfect excuse to capture moments, sublime glances and unforgettable moments and weave them all into an elaborate and enticing storyline.',
+},
+{
+  icon: 'assets/img/icons/icons8-literature-100.png',
+  title: 'Literary',
+  subtitle: 'This is the journalism based event of Spring Fest where the participants strive to bring about at least a small change in the society around us with the power of their pens.',
+},
+{
+  icon: 'assets/img/icons/icons8-comedy-100.png',
+  title: 'Humor Fest',
+  subtitle: 'Spring Fest strives to celebrate all that is worth spreading joy in our world, and in the same spirit, we bring to you a spectacular event in the search for the next big name in Indian Comedy.',
+},
+{
+  icon: 'assets/img/icons/icons8-ballet-dancer-100.png',
+  title: 'Music/Dance',
+  subtitle: 'Platform for the best dancing troops across the country to flaunt their hypnotic moves. And, a beatboxing competition featuring budding beatboxers from all over India.',
+},
+{
+  icon: 'assets/img/icons/icons8-selfie-100.png',
+  title: 'Drama',
+  subtitle: 'To all the drama queens out there, this is your best chance to hog the lime light and own the stage!',
+}];
+
+export const guests = [
+  {
+    image: 'assets/img/guest/1.png',
+    name: 'Sanjeev Kapoor',
+    position: 'Founder of Sanjeev Kapoor Restaurant',
+    description: 'The most celebrated face of Indian Cuisine. An accomplished entrepreneur and television personality, he is the host of hit shows like Khana Khazana and Masterchef India & the recipient of Padma Shree',
+  },
+  {
+    image: 'assets/img/guest/3.png',
+    name: 'Gautam Gambhir',
+    position: 'Member of Lok Sabha',
+    description: 'One of the finest Indian batsmen to have graced the game, we are proud to welcome Gautom Gambhir A recipient of the Padma Shri and the Arjuna Award, his accolades speak for themselves.',
+  },
+  {
+    image: 'assets/img/guest/5.png',
+    name: 'Abhishek Upmanyu',
+    position: 'Standup Comedy',
+    description: 'Presenting the headliner for this year\'s Humorfest, one of the most popular Indian stand-up comics, Abhishek Upmanyu. His unwavering razor-sharp wit has delighted audiences all over the country',
+  },
+  {
+    image: 'assets/img/guest/6.png',
+    name: 'Salim Merchant',
+    position: 'Music Composer and Singer',
+    description: 'Composing for hit movies like Chak De! India, and Rab Ne Bana Di Jodi, his talent speaks for itself. His time on Indian idol, Indian Idol Jr as well the Voice India have made him more popular',
+  },
+  {
+    image: 'assets/img/guest/2.png',
+    name: 'Smriti Irani',
+    position: 'Minister of Women and Child Development of India',
+    description: 'An accomplished actress who has won many laurels for her work on the screen, she joined the BJP in 2003 and has since become one of the most prominent figures in the party',
+  },
+  {
+    image: 'assets/img/guest/4.png',
+    name: 'Suresh Prabhu',
+    position: 'Minister of Commerce & Industry of India',
+    description: 'A chartered accountant by profession, it was in the political sphere that he found his calling. A member of parliament and formerly a cabinet minister, served on the council of ministers under 2 PMs',
+  },
+];
+
