83b9f248cb090c264789f8e116deb039d49cf8b7
Apple <riyabulia12@gmail.com>
2022-07-27 18:48:15 +0530

    Fixed webhint and stylelint errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,7 @@ if (window.screen.width <= 768) {
 if (mainEvent) {
   mainEvent.innerHTML = eventList.map((event) => `<div class="row col-lg-2 event py-2 gap-1 gap-lg-4 text-center">
       <a class="col-2 col-lg-12 event-icon">
-         <img src="${event.icon}"/>
+         <img src="${event.icon}" alt="${event.alt}"/>
       </a>
       <h2 class="col-3 col-lg-12 event-title"> ${event.title} </h2>
       <p class=" col-6 col-lg-12 event-subtitle px-lg-3"> ${event.subtitle} </p>
@@ -48,7 +48,7 @@ if (mainEvent) {
 if (guest) {
   guest.innerHTML = guests.map((guest) => `<div class="row col-lg-6">
       <a class="col-6 col-md-3 col-lg-4 guest-image">
-         <img src="${guest.image}"/>
+         <img src="${guest.image}" aalt="${guest.alt}"/>
       </a>
       <div class="col-6 col-md-9 col-lg-8 py-2">
          <h3 class="guest-name">${guest.name}</h3>
